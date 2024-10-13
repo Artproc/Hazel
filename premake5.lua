4aa7516153d96ae51bf21519af39ec6f9c659248
artproc <artproc@yahoo.com>
2024-10-13 14:06:24 -0500

    Added precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -13,8 +13,12 @@ project "Hazel"
 	location "Hazel"
 	kind "SharedLib"
 	language "C++"
+
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
 
 	files
 	{
