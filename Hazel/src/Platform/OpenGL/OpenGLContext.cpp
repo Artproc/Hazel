c5022eac7da929f698337cbbb9d7f3922c627b55
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2025-02-10 14:39:40 -0600

    Our First Triangle

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -7,22 +7,27 @@
 
 namespace Hazel {
 
-	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
-		: m_WindowHandle(windowHandle)
-	{
-		HZ_CORE_ASSERT(m_WindowHandle, "Window handle is null!");
-	}
+    OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
+        : m_WindowHandle(windowHandle)
+    {
+        HZ_CORE_ASSERT(m_WindowHandle, "Window handle is null!");
+    }
 
-	void OpenGLContext::Init()
-	{
-		glfwMakeContextCurrent(m_WindowHandle);
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		HZ_CORE_ASSERT(status, "Failed to initialize Glad");
-	}
+    void OpenGLContext::Init()
+    {
+        glfwMakeContextCurrent(m_WindowHandle);
+        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+        HZ_CORE_ASSERT(status, "Failed to initialize Glad");
 
-	void OpenGLContext::SwapBuffers()
-	{
-		glfwSwapBuffers(m_WindowHandle);
-	}
+        HZ_CORE_INFO("OpenGL Info:");
+        HZ_CORE_INFO("  Vender: {0}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
+        HZ_CORE_INFO("  Renderer: {0}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
+        HZ_CORE_INFO("  Version: {0}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
+    }
+
+    void OpenGLContext::SwapBuffers()
+    {
+        glfwSwapBuffers(m_WindowHandle);
+    }
 
 }
