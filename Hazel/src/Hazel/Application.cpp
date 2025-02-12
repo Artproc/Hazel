afdc91bc478ef4a2d7601bb49e5e35aa906559e6
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2025-02-12 14:18:28 -0600

    Added RendererAPI and RenderCommand architecture.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,11 +3,11 @@
 #include "Hazel/Log.h"
 #include "Hazel/Input.h"
 
-#include <Glad/glad.h>
+#include "Hazel/Renderer/Renderer.h"
 
 namespace Hazel {
 
-	//#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+	
 
 	Application* Application::s_Instance = nullptr;
 
@@ -166,16 +166,18 @@ namespace Hazel {
 		while (m_Running)
 		{
 
-			glClearColor(0.1f, 0.1f, 0.1f, 1);
-			glClear(GL_COLOR_BUFFER_BIT);
+			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+			RenderCommand::Clear();
+
+			Renderer::BeginScene();
 
 			m_BlueShader->Bind();
-			m_SquareVA->Bind();
-			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_SquareVA);
 
 			m_Shader->Bind();
-			m_VertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_VertexArray);
+
+			Renderer::EndScene();
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
