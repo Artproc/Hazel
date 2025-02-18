23dec45681dbf8310b88a4fc4404a3dea7a73da6
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2025-02-17 19:01:06 -0600

    Added basic batching to Renderer2D

--- Hazel/src/Hazel/Renderer/RendererAPI.h
+++ Hazel/src/Hazel/Renderer/RendererAPI.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Hazel/Core/Core.h"
 #include <glm/glm.hpp>
 
 #include "Hazel/Renderer/VertexArray.h"
@@ -19,7 +19,8 @@ namespace Hazel {
 		virtual void SetClearColor(const glm::vec4& color) = 0;
 		virtual void Clear() = 0;
 
-		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray) = 0;
+		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) = 0;
+		
 
 		inline static API GetAPI() { return s_API; }
 		static Scope<RendererAPI> Create();
