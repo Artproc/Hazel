ca419dca77903b418b0da1a9979c12f31a418370
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2025-02-23 20:00:42 -0600

    Entity class

--- Hazel/src/Hazel/Scene/Scene.cpp
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -5,6 +5,7 @@
 #include "Hazel/Renderer/Renderer2D.h"
 
 #include <glm/glm.hpp>
+#include "Entity.h"
 
 namespace Hazel {
 
@@ -41,9 +42,14 @@ namespace Hazel {
 	{
 	}
 
-	entt::entity Scene::CreateEntity()
+	Entity Scene::CreateEntity(const std::string& name)
 	{
-		return  m_Registry.create();
+		Entity entity = { m_Registry.create(), this };
+		entity.AddComponent<TransformComponent>();
+		auto& tag = entity.AddComponent<TagComponent>();
+		tag.Tag = name.empty() ? "Entity" : name;
+		
+		return entity;
 	}
 
 	void Scene::OnUpdate(Timestep ts)
