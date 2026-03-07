914c61f40c637046d046dadec876710e3c54244f
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2026-03-06 23:57:39 -0600

    Added premake and generated projects.

---
+++ premake5.lua
@@ -0,0 +1,122 @@
+workspace "Hazel"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	project "Hazel"
+		location "Hazel"
+		kind "SharedLib"
+		language "C++"
+		
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"%{prj.name}/src",
+			"%{prj.name}/vendor/spdlog/include"
+		}
+
+		filter "system:windows"
+			cppdialect "C++latest"
+			staticruntime "off"
+			systemversion "latest"
+
+			defines
+			{
+				"HZ_PLATFORM_WINDOWS",
+				"HZ_BUILD_DLL",
+				"GLFW_INCLUDE_NONE"
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+			}
+
+		filter "configurations:Debug"
+			defines "HZ_DEBUG"
+			runtime "Debug"
+			symbols "on"
+			buildoptions "/utf-8"
+
+		filter "configurations:Release"
+			defines "HZ_RELEASE"
+			runtime "Release"
+			optimize "on"
+			buildoptions "/utf-8"
+
+		filter "configurations:Dist"
+			defines "HZ_DIST"
+			runtime "Release"
+			optimize "on"
+			buildoptions "/utf-8"
+
+	project "Sandbox"
+		location "Sandbox"
+		kind "ConsoleApp"
+		language "C++"
+		
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"Hazel/vendor/spdlog/include",
+			"Hazel/src"
+		}
+
+		links
+		{
+			"Hazel"
+		}
+
+		filter "system:windows"
+			cppdialect "C++latest"
+			staticruntime "off"
+			systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "HZ_DEBUG"
+			runtime "Debug"
+			symbols "on"
+			buildoptions "/utf-8"
+
+		filter "configurations:Release"
+			defines "HZ_RELEASE"
+			runtime "Release"
+			optimize "on"
+			buildoptions "/utf-8"
+
+		filter "configurations:Dist"
+			defines "HZ_DIST"
+			runtime "Release"
+			optimize "on"
+			buildoptions "/utf-8"
