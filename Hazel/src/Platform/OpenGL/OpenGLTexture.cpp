80350517a39c0f90838ce080459289b91ff7f2c4
Arthur Proctor <109313647+Artproc@users.noreply.github.com>
2026-03-14 17:27:15 -0500

    Renderer2D now uses a single shader to render both textured and non-textured quads

--- Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -6,6 +6,23 @@
 
 namespace Hazel {
 
+
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		: m_Width(width), m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
 		: m_Path(path)
 	{
@@ -29,6 +46,10 @@ namespace Hazel {
 			internalFormat = GL_RGB8;
 			dataFormat = GL_RGB;
 		}
+
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
+
 		HZ_CORE_ASSERT(internalFormat & dataFormat, "Unsupported image format!");
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
@@ -50,8 +71,21 @@ namespace Hazel {
 		glDeleteTextures(1, &m_RendererID);
 	}
 
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		HZ_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
+	}
+
 	void OpenGLTexture2D::Bind(uint32_t slot) const
 	{
 		glBindTextureUnit(slot, m_RendererID);
+	}
+
+	void OpenGLTexture2D::Unbind(uint32_t slot) const
+	{
+			glBindTextureUnit(slot, 0);
 	}
 }
